adbfdba932b21bd8955e6be7168bf5c031274426
Zakaria Faizi <faizi.channel@gmail.com>
2020-11-16 13:34:13 -0500

    sprint 2-1

---
+++ Elearning/Models/Email.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Elearning.Models
+{
+    public class Email
+    {
+        public string To 
+        {
+            get; 
+            set; 
+        }
+
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string Message
+        {
+            get;
+            set;
+        }
+    }
+}
